aba3eaca1ca2cae667647589563408224c820e23
Claudwin <claudwin.fortune@gmail.com>
2018-12-04 20:40:39 +0000

    Added edit and delete functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,10 +18,22 @@ class PlacesController < ApplicationController
     end
    
     def edit 
+        @place = Place.find(params[:id])
+    end
+
+    def update
+        @place = Place.find(params[:id])
+        @place.update_attributes(place_params)
+        redirect_to root_path
+    end    
+
+    def destroy
+        @place = Place.find(params[:id])
+        @place.destroy
+        redirect_to root_path
+    end
     
-    end
-        
-    end
+    
     private
 
     def place_params
