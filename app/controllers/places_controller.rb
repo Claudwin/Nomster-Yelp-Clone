afb7d0b6ea92f416f364c2eaf79f4a864c30bef2
Claudwin <claudwin.fortune@gmail.com>
2018-12-05 20:59:50 +0000

    Added more user authentication for edit & update feature

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
     def index
         @places = Place.all
     end
@@ -19,6 +20,10 @@ class PlacesController < ApplicationController
    
     def edit 
         @place = Place.find(params[:id])
+
+        if @place.user != current_user
+            return render plain: 'Not Allowed', status: :forbidden
+        end
     end
 
     def update
