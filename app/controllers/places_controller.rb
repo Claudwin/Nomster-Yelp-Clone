96f23e46f50ca9254046c1b20bad0d7d330b9ac5
Claudwin <claudwin.fortune@gmail.com>
2018-12-05 21:59:22 +0000

    Added form validation for new place, name field

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
     end
 
     def create
-        current_user.places.create(place_params)
-        redirect_to root_path
+        @place = current_user.places.create(place_params)
+        if @place.valid?
+            redirect_to root_path
+        else 
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def show
