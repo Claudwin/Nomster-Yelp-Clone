24f0b183a9dd44839e65351c672bf80dd86eabe9
Claudwin <claudwin.fortune@gmail.com>
2018-12-12 01:46:12 +0000

    Added validation to rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+
+    validates :comments, presence: true, length: { maximum: 140, minimum: 3 }
+    validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
