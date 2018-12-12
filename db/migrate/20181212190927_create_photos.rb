84aedea062e412347e5163fea0fb8b5da96d038b
Claudwin <claudwin.fortune@gmail.com>
2018-12-12 19:30:16 +0000

    Added model for photos

---
+++ db/migrate/20181212190927_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :photos do |t|
+      t.text :caption
+      t.integer :user_id
+      t.integer :place_id
+      t.timestamps
+    end
+  end
+end
