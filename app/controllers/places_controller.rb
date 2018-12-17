9a75cab9f7a249778b687df15eca92dd6c85556b
Claudwin <claudwin.fortune@gmail.com>
2018-12-17 16:08:11 +0000

    Changed controller from pictures to photos

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,6 @@ class PlacesController < ApplicationController
 
     def index
         @places = Place.all
-        @comments_all = Comment.all 
     end
 
     def new
@@ -22,7 +21,7 @@ class PlacesController < ApplicationController
     def show
         @place = Place.find(params[:id])
         @comment = Comment.new
-        @picture = Photo.new
+        @photo = Photo.new
     end
    
     def edit 
