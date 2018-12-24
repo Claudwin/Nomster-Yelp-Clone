b4dc325d21b8e1006b950fc6f29136ecbf724c5c
Claudwin <claudwin.fortune@gmail.com>
2018-12-24 20:47:48 +0000

    Added email automated code for notification_mailer.rb

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
 default from: "no-reply@nomsterapp.com" 
 
-def comment_added
-    mail(to: "claudwin.fortune@gmail.com", 
+def comment_added(comment)
+        @place = comment.place
+        @place_owner = @place.user
+    mail(to: @place_owner.email, 
         subject: "A new comment was added" )
         
 end
