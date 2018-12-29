0bfdfb60f3c78005f5d2d5c7f3cd88609f523726
Claudwin <claudwin.fortune@gmail.com>
2018-12-29 15:42:27 +0000

    Fixed adding comment issue, & completed testing comment email with gmail server

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,12 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    after_create :send_comment_email
 
-    validates :comments, presence: true, length: { maximum: 140, minimum: 3 }
+    validates :comment, presence: true, length: { maximum: 140, minimum: 3 }
     validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
+    def send_comment_email
+        NotificationMailer.comment_added(self).deliver_now
+    end
 end
